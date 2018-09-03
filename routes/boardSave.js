1517254cb04734d2901265722da881d596ceabc7
박소현 <39909044+sohyeonpark0901@users.noreply.github.com>
2018-09-03 15:54:46 +0900

    게시물 저장 수정

--- routes/boardSave.js
+++ routes/boardSave.js
@@ -47,7 +47,7 @@ router.post('/',upload.array('userfile',15), function(req,res){
 
 
     let sql='INSERT INTO board_db (title,content,department) VALUES (?,?,?)';
-    let sqlFile = 'INSERT INTO file_table (keyNum,fileName,fileDirectory,department) VALUES ?';
+    let sqlFile = 'INSERT INTO file_table (keyNum,fileName) VALUES ?';
 
     let title=req.body.title;
     let content=req.body.content;
@@ -76,7 +76,7 @@ router.post('/',upload.array('userfile',15), function(req,res){
 
            else {
              console.log(result);
-                await req.files.map(Data => Value.push([result.insertId,Data.filename,Data.path,department]))
+                await req.files.map(Data => Value.push([result.insertId,Data.filename]))
                 await connection.query(sqlFile,[Value],function(err){
                  if(err){
                     console.log(err);
